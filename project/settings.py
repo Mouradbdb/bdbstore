405df70a695ecad3e607043cc3c6849693d7449c
Mouradbdb <beddibimourad@gmail.com>
2023-02-25 14:38:38 +0100

    store and carts functionality

--- project/settings.py
+++ project/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'STORE',
     'category',
     'accounts',
+    'carts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -65,7 +66,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'category.context_processors.menu_links'
+                'category.context_processors.menu_links',
+                'carts.context_processors.counter',
+
             ],
         },
     },
--- project/views.py
+++ project/views.py
@@ -3,6 +3,7 @@ from STORE.models import Product
 
 def home(request):
     products = Product.objects.all().filter(is_avaliable = True)
+    
     context = {
         'products':products
     }
